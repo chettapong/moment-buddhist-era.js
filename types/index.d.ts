61ccbbaec933019575d4f2f98a803990fa2c2f72
Chettapong Pinsuwan <chettapong.work@gmail.com>
2024-09-19 13:10:16 +0700

    chore: format type

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,9 @@
+import * as moment from 'moment'
+
 declare module 'moment' {
-  interface Moment extends Object {
+  interface Moment {
+    buddhistEra(): boolean
+    buddhistEra(value: boolean): moment.Moment
     format(format?: string, options?: { be?: boolean }): string
   }
 }
